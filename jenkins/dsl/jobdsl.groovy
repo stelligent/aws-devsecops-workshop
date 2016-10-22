48147549bf40bd0060d97467e94ed7ea5c82a7b3
Robert Murphy <rmurphy20@gmail.com>
2016-10-22 17:51:31 -0400

    Fixed style and added scm trigger

--- jenkins/dsl/jobdsl.groovy
+++ jenkins/dsl/jobdsl.groovy
@@ -1,21 +1,27 @@
 pipelineJob('aws-devsecops-workshop') {
-    displayName('AWS DevSecOps Workshop Pipeline')
+  displayName('AWS DevSecOps Workshop Pipeline')
 
-    description('An example pipeline showcasing the deployment of an application with a security focused pipeline.')
+  description('An example pipeline showcasing the deployment of an application with a security focused pipeline.')
 
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    branch('master')
-                    remote {
-                        credentials('jenkins')
-                        url('git@github.com:stelligent/aws-devsecops-workshop.git')
-                    }
-                }
-            }
-
-            scriptPath('Jenkinsfile')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          branch('master')
+          remote {
+            credentials('jenkins')
+            url('git@github.com:stelligent/aws-devsecops-workshop.git')
+          }
         }
+      }
+
+      scriptPath('Jenkinsfile')
     }
+  }
+
+  triggers {
+    scm('* * * * *') {
+      ignorePostCommitHooks(true)
+    }
+  }
 }
